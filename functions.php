3ac3f1948094b3ad200b7ddc454b48eeba958571
Nakamura <yn524967@gmail.com>
2024-05-19 16:33:23 +0900

    カスタムメニュー機能でグローバルナビとフッターナビを表示

--- functions.php
+++ functions.php
@@ -1,7 +1,16 @@
 <?php
-function my_enqueue_scripts() {
+function my_enqueue_scripts()
+{
     wp_enqueue_script('jquery');
-    wp_enqueue_script('bundle_js', get_template_directory_uri(). '/assets/js/bundle.js', array());
-    wp_enqueue_style('my_styles', get_template_directory_uri(). '/assets/css/styles.css', array());
+    wp_enqueue_script('bundle_js', get_template_directory_uri() . '/assets/js/bundle.js', array());
+    wp_enqueue_style('my_styles', get_template_directory_uri() . '/assets/css/styles.css', array());
 }
 add_action('wp_enqueue_scripts', 'my_enqueue_scripts');
+
+//ヘッダー、フッターのカスタムメニュー化
+register_nav_menus(
+    array(
+        'place_global' => 'グローバル',
+        'place_footer' => 'フッターナビ',
+    )
+);
