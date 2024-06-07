e278a9a4289a99f1e6c2ef8adf28deb04ce7c79a
Nakamura <yn524967@gmail.com>
2024-06-07 10:56:41 +0900

    投稿ページのナビゲーションを動的化

--- content-single.php
+++ content-single.php
@@ -6,10 +6,16 @@
   </div>
 </div>
 <div class="more-news">
-  <div class="next">
-    <a class="another-link" href="#">NEXT</a>
-  </div>
+  <?php 
+  $next_post = get_next_post();
+  $prev_post = get_previous_post();
+  if($next_post):
+  ?>
   <div class="prev">
-    <a class="another-link" href="#">PREV</a>
+    <a class="another-link" href="<?php echo get_permalink($prev_post->ID); ?>">PREV</a>
   </div>
+  <div class="next">
+    <a class="another-link" href="<?php echo get_permalink($next_post->ID); ?>">NEXT</a>
+  </div>
+  <?php endif; ?>
 </div>
